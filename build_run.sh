b9bc584604c63e4b10b05f451b3e150694cc3d2c
langdon <langdon@fedoraproject.org>
2015-05-14 22:12:19 -0400

    ok, working, next is to clean things up and get some examples posted to nulecule

--- build_run.sh
+++ build_run.sh
@@ -2,11 +2,12 @@
 echo "Note: you may need to add your docker-registry to /etc/sysconfig/docker "
 echo "(on CentOS/RHEL/Fedora) for this to push to the registries correctly"
 
+
 if [ -z "$USERNAME" ]; then
     echo "setting USERNAME to " `whoami`
     USERNAME=`whoami`
 fi
-
+exit()
 if [ -z "$DOCKER_REGISTRY" ]; then
     echo "setting DOCKER_REGISTRY to localhost:5000"
     DOCKER_REGISTRY=localhost:5000
@@ -20,39 +21,43 @@ echo "building mariadb"
 cd drupal-app/graph/mariadb-app/graph/mariadb
 #docker build --rm -t $USERNAME/maria --file="docker-artifacts/Dockerfile" .
 docker build -t $USERNAME/mariadb --file="Dockerfile" .
-docker tag -f $USERNAME/mariadb $DOCKER_REGISTRY/mariadb
-docker push $DOCKER_REGISTRY/mariadb
+docker tag -f $USERNAME/mariadb $DOCKER_REGISTRY/mariadb$TAG
+echo "pushing to $DOCKER_REGISTRY/mariadb$TAG"
+docker push $DOCKER_REGISTRY/mariadb$TAG
 #docker rmi $USERNAME/maria
 echo "build complete: mariadb"
 cd $BASE_DIR
 
-#build mariadb-app docker container
-echo "building mariadb-app"
+#build mariadb-atomicapp  docker container
+echo "building mariadb-atomicapp"
 cd drupal-app/graph/mariadb-app
-docker build -t $USERNAME/mariadb-app --file="docker-artifacts/Dockerfile" .
-#docker build --no-cache -t $USERNAME/mariadb-app --file="docker-artifacts/Dockerfile" .
-docker tag -f $USERNAME/mariadb-app $DOCKER_REGISTRY/mariadb-app
-docker push $DOCKER_REGISTRY/mariadb-app
-#docker rmi $USERNAME/mariadb-app
-echo "build complete: mariadb-app"
+docker build -t $USERNAME/mariadb-atomicapp --file="docker-artifacts/Dockerfile" .
+#docker build --no-cache -t $USERNAME/mariadb-atomicapp  --file="docker-artifacts/Dockerfile" .
+docker tag -f $USERNAME/mariadb-atomicapp $DOCKER_REGISTRY/mariadb-atomicapp$TAG
+echo "pushing to $DOCKER_REGISTRY/mariadb$TAG"
+echo "y" | docker push $DOCKER_REGISTRY/mariadb-atomicapp$TAG
+#docker rmi $USERNAME/mariadb-atomicapp
+echo "build complete: mariadb-atomicapp"
 cd $BASE_DIR
 
 #build drupal docker container
 echo "building drupal"
 cd drupal-app/graph/drupal
 docker build -t $USERNAME/drupal --file="Dockerfile" .
-docker tag -f $USERNAME/drupal $DOCKER_REGISTRY/drupal
-docker push $DOCKER_REGISTRY/drupal
+docker tag -f $USERNAME/drupal $DOCKER_REGISTRY/drupal$TAG
+echo "pushing to $DOCKER_REGISTRY/mariadb$TAG"
+echo "y" | docker push $DOCKER_REGISTRY/drupal$TAG
 echo "build complete: drupal"
 cd $BASE_DIR
 
-#build drupal-app docker container
-echo "building drupal-app"
+#build drupal-atomicapp  docker container
+echo "building drupal-atomicapp"
 cd drupal-app/
-docker build -t $USERNAME/drupal-app --file="docker-artifacts/Dockerfile" .
-docker tag -f $USERNAME/drupal-app $DOCKER_REGISTRY/drupal-app
-docker push $DOCKER_REGISTRY/drupal-app
-echo "build complete: drupal-app"
+docker build -t $USERNAME/drupal-atomicapp  --file="docker-artifacts/Dockerfile" .
+docker tag -f $USERNAME/drupal-atomicapp  $DOCKER_REGISTRY/drupal-atomicapp$TAG
+echo "pushing to $DOCKER_REGISTRY/mariadb$TAG"
+echo "y" | docker push $DOCKER_REGISTRY/drupal-atomicapp$TAG
+echo "build complete: drupal-atomicapp"
 cd $BASE_DIR
 
 #echo docker build --rm -t $USERNAME/atomicapp-run .
